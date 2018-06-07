3fd4f8b13e8834721da37d18dfeb3ebdf6adcde0
KEVIN OBRIEN <kevin.eugene.obrien@gmail.com>
2018-06-07 17:22:14 -0600

    :sparkles: Added migration for job

--- migrations/20180605131732_job.js
+++ migrations/20180605131732_job.js
@@ -1,8 +1,20 @@
-
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable("job", (table) => {
+    table.increments();
+    table.integer("customer_id").notNullable();
+    table.text("company");
+    table.text("title");
+    table.text("location");
+    table.text("salary");
+    table.text("URL");
+    table.text("notes");
+    table.dateTime("deadline");
+    table.dateTime("applied");
+    table.dateTime("interview1");
+    table.dateTime("interview2");
+    table.text("offer");
+  });
 };
-
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists("job");
 };
