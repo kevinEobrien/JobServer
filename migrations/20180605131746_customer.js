fdedb50802a79f440c8345bcebd9d039f6b46973
KEVIN OBRIEN <kevin.eugene.obrien@gmail.com>
2018-06-08 18:54:17 -0600

    added customer migration

--- migrations/20180605131746_customer.js
+++ migrations/20180605131746_customer.js
@@ -1,8 +1,12 @@
-
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable("customer", (table) => {
+    table.increments();
+    table.text("username").notNullable();
+    table.text("password");
+    table.text("name");
+  });
 };
-
-exports.down = function(knex, Promise) {
   
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists("customer");
 };
